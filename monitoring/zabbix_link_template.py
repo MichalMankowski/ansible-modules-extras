4386e74abdf9d2d54774b8c3a91bc1d842e1ba0a
michalma <michal.mankowski@intel.com>
2016-10-18 19:31:14 +0200

    GPLv3 license header, RETURN

--- monitoring/zabbix_link_template.py
+++ monitoring/zabbix_link_template.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+# (c) 2016, Michal Mankowski <michal.mankowski () intel.com>
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
 from ansible.module_utils.basic import *
 try:
     from pyzabbix import ZabbixAPI
@@ -14,7 +31,7 @@ short_description: Link/unlink given hosts list with given templates list
 description:
   - Link/unlink given hosts list with given templates list
 
-version_added: "2.2"
+version_added: "2.3"
 author: "Michal Mankowski (@MichalMankowski)"
 requirements:
   - "python >= 2.6"
@@ -58,6 +75,9 @@ EXAMPLES = '''
     host:
     - "Zabbix server"
     state: present
+'''
+RETURN = '''
+#defaults
 '''
 
 
